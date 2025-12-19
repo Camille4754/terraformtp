1a19f6eaacc947af42948131cf5598b17a26684c
Camille4754 <camille.chebi@viacesi.fr>
2025-12-19 16:49:13 +0100

    Update network.tf

---
+++ network.tf
@@ -0,0 +1,35 @@
+resource "google_compute_network" "vpc" {
+  name                    = "tp-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "tp-subnet"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+  ip_cidr_range = "10.0.0.0/24"
+}
+
+resource "google_compute_firewall" "ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "http" {
+  name    = "allow-http"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
